3c075d63766220f0fd6df9c7ef74d09cbdecec7e
paul-r <paulrousseaucs@gmail.com>
2024-01-23 09:25:28 +0100

    [FEAT] Quantity of the products in home are updated according to a sale

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { GetServerSideProps } from "next/types";
 import ProductLayout from "@/components/ProductLayout/ProductLayout";
 import CashHeaderLayout from "@/components/CashHeaderLayout/CashHeaderLayout";
 import prisma from "@/lib/prisma";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export interface Product {
   id: number;
@@ -21,11 +21,28 @@ export interface ProductSaleRecap {
 
 export default function Cash({ products }: { products: Product[] }) {
   const [productsToSale, setProductsToSale] = useState<ProductSaleRecap[]>([]);
+  const [productsToShow, setProductsToShow] = useState<Product[]>(products);
 
   const handleProductToSale = (productId: number, addOne: Boolean) => {
     const alreadyInCart = productsToSale.find(
       (product) => product.id === productId
     );
+
+    // Update the numberInStock of the product
+    const newProductsToShow = productsToShow.map((product: Product) => {
+      if (product.id === productId) {
+        const addOrRemove = addOne ? 1 : -1;
+        if (
+          product.numberInStock - addOrRemove < 0 ||
+          (!alreadyInCart && !addOne)
+        )
+          return product;
+        product.numberInStock = product.numberInStock - addOrRemove;
+      }
+      return product;
+    });
+
+    setProductsToShow(newProductsToShow);
 
     if (alreadyInCart) {
       if (alreadyInCart.quantity === 1 && !addOne) {
@@ -69,7 +86,7 @@ export default function Cash({ products }: { products: Product[] }) {
     <div>
       <CashHeaderLayout productsToSale={productsToSale} />
       <ProductLayout
-        products={products}
+        products={productsToShow}
         handleProductToSale={handleProductToSale}
       />
     </div>
