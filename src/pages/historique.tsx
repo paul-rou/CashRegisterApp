311e64b629d262e3c17a61633953b88549e8c2d3
paul-r <paulrousseaucs@gmail.com>
2024-05-06 21:37:55 +0200

    [FEAT] First version of sale history page, just a list of sales with no style

--- src/pages/historique.tsx
+++ src/pages/historique.tsx
@@ -1,9 +1,40 @@
-export default function History() {
-    return (
-      <>
-        <h1>History page</h1>
-        <p>To see what has been sold</p>
-      </>
-    )
-  }
-  
+import prisma from "@/lib/prisma";
+import { GetStaticProps } from "next";
+
+export interface Sell {
+  id: number;
+  date: string;
+  productName: string;
+  price: number;
+  numberSold: number;
+  paymentMethod: string;
+  marketLocation: string;
+}
+
+export default function History({ sell }: { sell: Sell[] }) {
+  return (
+    <>
+      <h1>History page</h1>
+      <div className="flex flex-col">
+        {sell?.map((sell) => (
+          <div key={sell.id}>
+            <p>{sell.date}</p>
+            <p>{sell.productName}</p>
+            <p>{sell.price}</p>
+            <p>{sell.numberSold}</p>
+            <p>{sell.paymentMethod}</p>
+            <p>{sell.marketLocation}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const sell = await prisma.sell.findMany();
+
+  return {
+    props: { sell },
+  };
+};
